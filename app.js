f2306b137f9d8b8839bf2cd1a8f248daf980266e
toucanairV2 <toucanairIT@gmail.com>
2019-07-29 16:03:49 -0400

    first node

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const configMessage = require("./configMessage");
+
+
+const app = express(); /*instancia express*/
+app.use(cors()); /*Middlewares*/
+app.use(bodyParser.json());
+
+
+app.post("/formulario", (req, res) => { /*ENDPOINT*/
+    //console.log(req.body);
+    configMessage(req.body);
+    res.status(200).send();
+});
+
+
+app.listen(3000, () => { /*Escucha de nuestro server*/
+    console.log("Server started on port 3000!");
+});
---
+++ configMessage.js
@@ -0,0 +1,40 @@
+const nodemailer = require("nodemailer");
+const smtpTransport = require('nodemailer-smtp-transport');
+
+
+module.exports = (formulario) => {
+    let transporter = nodemailer.createTransport(smtpTransport({
+    host:'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'toucanairit@gmail.com', // Cambialo por tu email
+        pass: 'Toucanairllc2019.' // Cambialo por tu password
+    },
+    tls:{
+        rejectUnauthorized:false
+    }
+    }));
+   let mailOptions = {
+    from: 'toucanairit@gmail.com', //'"${formulario.nombre}” <${formulario.email}>',
+    to: "barbara29rp@gmail.com", // Cambia esta parte por el destinatario
+    subject: "NewPage Contact Request",
+    html: `
+        <p> You have a New Contact Request </p>
+        <ul>
+            <li><strong>Name:</strong> ${formulario.name}</li>
+            <li><strong>E-mail:</strong> ${formulario.email}</li>
+            <li><strong>Phone:</strong> ${formulario.phone}</li>
+            <li><strong>Company:</strong> ${formulario.company}</li>
+        </ul>
+        <h3><strong>Message:</strong> ${formulario.message}</h3>
+        `};
+
+
+   transporter.sendMail(mailOptions, function (err, info) {
+    if (err)
+    console.log(err)
+    else
+    console.log('Email sent: ' + info.response);
+    });
+   }
