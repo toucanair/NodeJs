9a9cfb5fb1e750e7064d49af6351e27a94713eaa
toucanairV2 <toucanairIT@gmail.com>
2019-09-17 10:30:11 -0400

    emails again

--- configMessage.js
+++ configMessage.js
@@ -17,7 +17,7 @@ module.exports = (formulario) => {
     }));
    let mailOptions = {
     from: 'toucanairit@gmail.com', //'"${formulario.nombre}” <${formulario.email}>',
-    to: "barbara29rp@gmail.com, juan@toucanair.com, zuarfer@gmail.com, sales@toucanair.com", //  destinatario
+    to: "zuarfer@gmail.com", //  destinatario
     subject: "NewPage Contact Request",
     html: `
         <p> You have a New Contact Request </p>
